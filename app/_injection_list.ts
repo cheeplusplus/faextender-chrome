33598edd694cb47053268dfebdcc46df65724fd1
Kauko <andrew@andrewneo.com>
2025-09-12 01:14:12 -0700

    Update injection points
    Closes #36

--- app/_injection_list.ts
+++ app/_injection_list.ts
@@ -24,6 +24,8 @@ interface InjectionPoints {
     openInGDocsInsertPosition: HTMLElement;
 }
 
+export type InjectionPoint = keyof InjectionPoints;
+
 const InjectionPointList: InjectionVersions = {
     "classic": {
         "downloadInsertPosition": "#page-submission .maintable:first th.cat",
@@ -33,7 +35,7 @@ const InjectionPointList: InjectionVersions = {
         "miniGallery": "#page-submission div.minigalleries",
         "journalHighlightMatch": "#messages-journals ul.message-stream li",
         "submissionHighlightMatch": "#messagecenter-submissions section.gallery figure",
-        "standardSubmissionLink": "figure figcaption a[href*='/view/']",
+        "standardSubmissionLink": "figure a[href*='/view/']",
         "insertInTabsInsertPositionSubmissions": "#messagecenter-submissions div.actions",
         "insertInTabsInsertPositionGallery": "#page-galleryscraps div.page-options",
         "insertInTabsInsertPositionFavorites": "#favorites table.maintable>tbody>tr>td>table>tbody>tr:nth-child(1)>td:nth-child(1)",
@@ -45,10 +47,10 @@ const InjectionPointList: InjectionVersions = {
         "artistLink": "#submission_page div.submission-content div.submission-id-sub-container a[href*='/user/']",
         "addFavoriteLink": "a[href^='/fav/']:contains('+ Fav')",
         "miniGallery": "#submission_page section.minigallery-more div.preview-gallery",
-        "journalHighlightMatch": "#messagecenter-other div#messages-journals ul.message-stream li",
+        "journalHighlightMatch": "#messages-journals ul.message-stream li",
         "submissionHighlightMatch": "#messagecenter-submissions section.gallery figure",
-        "standardSubmissionLink": "figure figcaption a[href*='/view/']",
-        "insertInTabsInsertPositionSubmissions": "#messagecenter-new-submissions div.section-body>div",
+        "standardSubmissionLink": ".gallery-section figure a[href*='/view/']",
+        "insertInTabsInsertPositionSubmissions": "#messagecenter-new-submissions .actions-section div.aligncenter",
         "insertInTabsInsertPositionGallery": "#page-galleryscraps div.submission-list div.aligncenter",
         "insertInTabsInsertPositionFavorites": "#standardpage div.aligncenter",
         "openInGDocsInsertPosition": "#submission_page section.buttons"
