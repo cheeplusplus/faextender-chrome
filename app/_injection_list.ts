673e650f9582c55989f97d91ef16398027a98ff7
Kauko <andrew@andrewneo.com>
2026-05-12 21:33:34 -0700

    Use the lower options buttons instead

--- app/_injection_list.ts
+++ app/_injection_list.ts
@@ -43,9 +43,9 @@ const InjectionPointList: InjectionVersions = {
     },
     "beta": {
         "downloadInsertPosition": "#submission_page #submission-main-content > div:nth-child(2)",
-        "downloadLink": "#submission_page div.submission-controls-upper a:contains('Download')",
+        "downloadLink": "#submission_page #submission-options a[href*='d.furaffinity.net/']",
         "artistLink": "#submission_page div.submission-description-artist a[href*='/user/']",
-        "addFavoriteLink": "#submission_page div.submission-options a[href^='/fav/']",
+        "addFavoriteLink": "#submission_page #submission-options a[href^='/fav/']",
         "miniGallery": "#submission_page #minigallery",
         "journalHighlightMatch": "#messages-journals ul.message-stream li",
         "submissionHighlightMatch": "#messagecenter-submissions section.gallery figure",
